a12762d8c30c73a422c57c47c830d3e170eb6a74
TheGuiza <theguiza@gmail.com>
2025-05-26 13:27:23 -0700

    deploy full stack update with database and profile

--- Backend/3.1 Express Server/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-//import axios from "axios";
-// then to use this axios.function 
-const app = express(); 
-const port = 3000; 
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}.`);
-}); 
